aea80d9f1b276f65cf6f5972852a1537c9d322f2
Khalifa1997 <ahmed70000@gmail.com>
2019-12-25 02:44:10 +0200

    Changes

--- routes/Reservations.js
+++ routes/Reservations.js
@@ -1,32 +1,34 @@
-
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 const router = express.Router();
-const Event=require('./Models/Event').Event;
-const ReservationSchema = new mongoose.Schema({
-  ReservationId: {
-    type: String,
-    unique: true,
+const Event = require("./Models/Event").Event;
+const ReservationSchema = new mongoose.Schema(
+  {
+    ReservationId: {
+      type: String,
+      unique: true
+    },
+    Username: {
+      type: String
+    },
+    EventId: {
+      type: String
+    },
+    CreditCardNumber: {
+      type: String
+    },
+    Seats: [
+      {
+        type: String
+      }
+    ]
   },
-  Username: {
-    type: String
-  },
-  EventId: {
-    type: String
-  },
-  CreditCardNumber: {
-    type: String,
-  },
-  Seats: [{
-    type:String
-  }]
-}, { collection: 'Reservations' });
-const Reservation = mongoose.model('Reservation', ReservationSchema);
+  { collection: "Reservations" }
+);
+const Reservation = mongoose.model("Reservation", ReservationSchema);
 
-
-//get reservation by EventID 
-router.get('/', async (req, res) => {
-
+//get reservation by EventID
+router.get("/:ids", async (req, res) => {
   /*
   console.log(req.params);
   console.log(req.params.auth_name);
@@ -35,31 +37,25 @@ router.get('/', async (req, res) => {
  */
   //console.log(req.query.ReservationId); /// ONLY WORKING
 
-
-  mongoose.connection.collection("Reservations").findOne({ EventId: req.query.EventId },
-    (err, doc) => {
-
+  mongoose.connection
+    .collection("Reservations")
+    .findOne({ EventId: req.params.ids }, (err, doc) => {
       if (!doc || err) {
         //console.log(doc);
-        res.status(400).json({  // sends a json with 404 code
-          success: false,  // user not retrieved  
-          "Message": "Reservation not  found !"
+        res.status(400).json({
+          // sends a json with 404 code
+          success: false, // user not retrieved
+          Message: "Reservation not  found !"
         });
-      }
-      else {
+      } else {
         //console.log(doc);
         res.status(200).json(doc);
-
       }
-    }
-
-
-  )
+    });
 });
 
 //Create New Reservation
-router.post('/Create', async (req, res) => {
-
+router.post("/Create", async (req, res) => {
   var NewReservation = new Reservation();
   console.log(req.body);
   console.log(req.body.Seats);
@@ -68,51 +64,49 @@ router.post('/Create', async (req, res) => {
   NewReservation.EventId = req.body.EventId;
   NewReservation.CreditCardNumber = req.body.CreditCardNumber;
   NewReservation.Seats = req.body.Seats; //array of seat numbers
-console.log(NewReservation.Seats);
+  console.log(NewReservation.Seats);
 
   //TODO :UPDATE EVENT SEATS AS RESERVED
   //Seats provided here are already checked by frontend to be vacant
-  let ifevent = await Event.findOne({EventId:req.body.EventId});
-  if(!ifevent)
-  return res.status(400).json({Message:'Invalid Event Id Number'});
+  let ifevent = await Event.findOne({ EventId: req.body.EventId });
+  if (!ifevent)
+    return res.status(400).json({ Message: "Invalid Event Id Number" });
 
   console.log(ifevent);
-    //iterate on all seats 
-    NewReservation.Seats.forEach(seatnum => {
-      ifevent.Seats[parseInt(seatnum)].IsReserved=true;
-     
-    });
-    //remove old event
-    await Event.remove({EventId:ifevent.EventId});
-    //add updated Event
-    await Event.insertMany(ifevent);
+  //iterate on all seats
+  NewReservation.Seats.forEach(seatnum => {
+    ifevent.Seats[parseInt(seatnum)].IsReserved = true;
+  });
+  //remove old event
+  await Event.remove({ EventId: ifevent.EventId });
+  //add updated Event
+  await Event.insertMany(ifevent);
 
   NewReservation.save((err, doc) => {
     if (!err) {
-
       res.json({ "Reservation Added": true });
-    }
-    else {
-      res.json({ "Reservation Added": false, "error": err });
+    } else {
+      res.json({ "Reservation Added": false, error: err });
       // console.log('error during user insertion: ' + err);
     }
   });
-
 });
-
 
 //Remove Reservation by EventID and Username
-router.post('/Remove', async (req, res) => {
-  let isfound = await Reservation.findOne({ Username: req.body.Username, EventId: req.body.EventId });
+router.post("/Remove", async (req, res) => {
+  let isfound = await Reservation.findOne({
+    Username: req.body.Username,
+    EventId: req.body.EventId
+  });
   if (!isfound)
-    res.status(400).send({ "ReturnedMsg": "Reservation doesn't exist" });
+    res.status(400).send({ ReturnedMsg: "Reservation doesn't exist" });
 
-  let check = await Reservation.remove({ Username: req.body.Username, EventId: req.body.EventId });
-  if (check) return res.status(200).send({ "ReturnMsg": "Reservation Removed" });
-  else
-    res.status(400).send({ "ReturnedMsg": "error not removed" });
-
+  let check = await Reservation.remove({
+    Username: req.body.Username,
+    EventId: req.body.EventId
+  });
+  if (check) return res.status(200).send({ ReturnMsg: "Reservation Removed" });
+  else res.status(400).send({ ReturnedMsg: "error not removed" });
 });
-
 
 module.exports = router;
